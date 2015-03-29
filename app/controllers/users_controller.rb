0914336030e0c0575690ae613bd54c0a1434b990
leonidasm <ipad300512@gmail.gcom>
2015-03-29 21:46:00 +0300

    init

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+http_basic_authenticate_with name: "dhh", password: "secret1", except: [:create, :update, :new, :show]
   # GET /users
   # GET /users.json
   def index
